1e79c1d9baff90cda96c1402e3f672bc41229e8f
Hubert Graham <hubert.graham90@gmail.com>
2025-11-26 11:36:31 -0500

    worked on functions to calculate overdue loan

--- library/tasks.py
+++ library/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from .models import Loan
 from django.core.mail import send_mail
 from django.conf import settings
+import datetime
 
 @shared_task
 def send_loan_notification(loan_id):
@@ -18,3 +19,7 @@ def send_loan_notification(loan_id):
         )
     except Loan.DoesNotExist:
         pass
+
+@shared_task
+def check_overdue_loans():
+    overdueLoans =  Loan.objects.Filter(lambda x: x.is_returned == False and x.due_date < datetime.datetime.now())
