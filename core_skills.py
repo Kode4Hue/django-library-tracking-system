3fa77247e005cdf94a77f980d7591d28e4dc1929
Hubert Graham <hubert.graham90@gmail.com>
2025-11-26 11:22:47 -0500

    completed python core skills tasks

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,17 @@
 import random
-# rand_list =
+# rand_list = 
+n = 10
+rand_list = random.sample(range(1, 20), n)
+print(rand_list)
 
 # list_comprehension_below_10 =
+list_comprehension_below_10 = [x for x in rand_list if x < 10]
+print("filter list below 10 using list comprehension")
+print(list(list_comprehension_below_10))
 
-# list_comprehension_below_10 =
+# list_comprehension_below_10 = 
+list_comprehension_below_10 = [] # empty the list comprehension to ensure it is empty
+print("filter list below 10 using filter")
+# print(list_comprehension_below_10)
+list_comprehension_below_10 =  filter(lambda x: x < 10, rand_list)
+print(list(list_comprehension_below_10))
